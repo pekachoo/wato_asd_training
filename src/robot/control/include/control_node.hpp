8e0dbd831a22a4faf9a43003e07f6e9e162d5275
pekachoo <jliu6162@gmail.com>
2025-09-19 10:43:09 -0400

    node setup

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -2,6 +2,11 @@
 #define CONTROL_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
 
 #include "control_core.hpp"
 
@@ -11,6 +16,18 @@ class ControlNode : public rclcpp::Node {
 
   private:
     robot::ControlCore control_;
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
+    rclcpp::TimerBase::SharedPtr control_timer_;
+
+    // Data
+    nav_msgs::msg::Path::SharedPtr current_path_;
+    nav_msgs::msg::Odometry::SharedPtr robot_odom_;
+
+    // Main loop
+    void controlLoop();
+
 };
 
 #endif
--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -1,6 +1,23 @@
 #include "control_node.hpp"
 
-ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore(this->get_logger())) {}
+ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore(this->get_logger())) {
+  path_sub_ = this->create_subscription<nav_msgs::msg::Path>(
+      "/path", 10, [this](const nav_msgs::msg::Path::SharedPtr msg) { current_path_ = msg; });
+
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/odom/filtered", 10, [this](const nav_msgs::msg::Odometry::SharedPtr msg) { robot_odom_ = msg; });
+
+  cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+
+  // Timer
+  control_timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(100), [this]() { controlLoop(); });
+}
+
+void ControlNode::controlLoop(){
+
+}
+
 
 int main(int argc, char ** argv)
 {
