931d559cad47e0a9374f6f86e5d106940c9e04e6
pekachoo <jliu6162@gmail.com>
2025-09-18 20:44:57 -0400

    Update planner_node.cpp

--- src/robot/planner/src/planner_node.cpp
+++ src/robot/planner/src/planner_node.cpp
@@ -1,10 +1,8 @@
 #include "planner_node.hpp"
 
 PlannerNode::PlannerNode() : Node("planner"), planner_(robot::PlannerCore(this->get_logger())) {
-  // load ROS2 yaml parameters
   processParameters();
 
-  // Subscribers
   map_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
     "/map",
     10,
@@ -17,17 +15,14 @@ PlannerNode::PlannerNode() : Node("planner"), planner_(robot::PlannerCore(this->
     std::bind(&PlannerNode::goalCallback, this, std::placeholders::_1)
   );
 
-  // Subscribe to odometry from /odom/filtered
   odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
     "/odom/filtered",
     10,
     std::bind(&PlannerNode::odomCallback, this, std::placeholders::_1)
   );
 
-  // Publisher
   path_pub_ = this->create_publisher<nav_msgs::msg::Path>("/path", 10);
 
-  // Timer to check goal/timeout status periodically (500 ms)
   timer_ = this->create_wall_timer(
     std::chrono::milliseconds(500),
     std::bind(&PlannerNode::timerCallback, this)
