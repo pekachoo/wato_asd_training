7c0779d7c94e9bf674222b05d8c95ff8bafc863e
pekachoo <jliu6162@gmail.com>
2025-09-18 20:39:59 -0400

    planner node initializtaion

--- src/robot/planner/include/planner_node.hpp
+++ src/robot/planner/include/planner_node.hpp
@@ -1,7 +1,13 @@
 #ifndef PLANNER_NODE_HPP_
 #define PLANNER_NODE_HPP_
 
+#include <mutex>
+
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
 
 #include "planner_core.hpp"
 
@@ -9,8 +15,29 @@ class PlannerNode : public rclcpp::Node {
   public:
     PlannerNode();
 
+    void processParameters();
+
+    void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr map_msg);
+    void goalCallback(const geometry_msgs::msg::PointStamped::SharedPtr  goal_msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr odom_msg);
+    void timerCallback();
+
+    void pubPath();
+    void resetGoal();
+
   private:
     robot::PlannerCore planner_;
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr goal_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    nav_msgs::msg::OccupancyGrid current_map_;
+    geometry_msgs::msg::PointStamped goal_;
+    geometry_msgs::msg::Pose robot_pose_;
+
+
 };
 
 #endif 
--- src/robot/planner/src/planner_node.cpp
+++ src/robot/planner/src/planner_node.cpp
@@ -1,6 +1,40 @@
 #include "planner_node.hpp"
 
-PlannerNode::PlannerNode() : Node("planner"), planner_(robot::PlannerCore(this->get_logger())) {}
+PlannerNode::PlannerNode() : Node("planner"), planner_(robot::PlannerCore(this->get_logger())) {
+  // load ROS2 yaml parameters
+  processParameters();
+
+  // Subscribers
+  map_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
+    "/map",
+    10,
+    std::bind(&PlannerNode::mapCallback, this, std::placeholders::_1)
+  );
+
+  goal_sub_ = this->create_subscription<geometry_msgs::msg::PointStamped>(
+    "/goal_point",
+    10,
+    std::bind(&PlannerNode::goalCallback, this, std::placeholders::_1)
+  );
+
+  // Subscribe to odometry from /odom/filtered
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+    "/odom/filtered",
+    10,
+    std::bind(&PlannerNode::odomCallback, this, std::placeholders::_1)
+  );
+
+  // Publisher
+  path_pub_ = this->create_publisher<nav_msgs::msg::Path>("/path", 10);
+
+  // Timer to check goal/timeout status periodically (500 ms)
+  timer_ = this->create_wall_timer(
+    std::chrono::milliseconds(500),
+    std::bind(&PlannerNode::timerCallback, this)
+  );
+
+  // planner_.initPlanner(smoothing_factor_, iterations_);
+}
 
 int main(int argc, char ** argv)
 {
